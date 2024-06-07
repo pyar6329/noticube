42922da00cf4c35c7ef7fca244000a0a7c841414
Tsukasa Arima <pyar6329@gmail.com>
2024-06-07 17:34:39 +0000

    remove unused codes

--- scripts/build_docker_local.sh
+++ scripts/build_docker_local.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# required environment variables
-# - GITHUB_TOKEN: GitHub's access token
-# - AWS_DEFAULT_REGION: AWS region
-# - AWS_ACCESS_KEY_ID: AWS access key
-# - AWS_SECRET_ACCESS_KEY: AWS secret key
-# - DOCKER_BUILDKIT=1 : use buildkit
-# - PROTOC_VERSION: protoc version. ref: https://github.com/protocolbuffers/protobuf/releases
-
 set -e
 
 CURRENT_DIR=$(echo $(cd $(dirname $0) && pwd))
@@ -21,18 +13,11 @@ tag_name="dev-$(git rev-parse HEAD)"
 repo_name="$REGISTRY_URL/noticube"
 image_name="$repo_name:$tag_name"
 
-# create .git-credentials file by $GITHUB_TOKEN
-echo "https://x-access-token:${GITHUB_TOKEN}@github.com" > $PROJECT_ROOT/git-credentials.txt
-
 docker build \
   -t "$image_name" \
   -f $CURRENT_DIR/Dockerfile \
   --build-arg RUST_VERSION=${RUST_VERSION} \
   $PROJECT_ROOT
-
-if [ -e $PROJECT_ROOT/git-credentials.txt ]; then
-  rm -rf $PROJECT_ROOT/git-credentials.txt
-fi
 
 docker run --rm -it -d --name tmp_noticube -e "SLACK_BOT_TOKEN=foo" -e "SLACK_CHANNEL_ID=bar" ${image_name}
 
