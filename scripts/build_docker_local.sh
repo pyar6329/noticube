abf9eb8a5e025104e01803945fb0a49afab29ec7
Tsukasa Arima <pyar6329@gmail.com>
2024-06-07 16:11:12 +0000

    fix docker image

--- scripts/build_docker_local.sh
+++ scripts/build_docker_local.sh
@@ -17,7 +17,7 @@ REGISTRY_URL="ghcr.io/pyar6329"
 RUST_VERSION=$(cat ${PROJECT_ROOT}/rust-toolchain | tr -d '\n')
 
 # set git hash
-tag_name="tmp2-$(git rev-parse HEAD)"
+tag_name="dev-$(git rev-parse HEAD)"
 repo_name="$REGISTRY_URL/noticube"
 image_name="$repo_name:$tag_name"
 
@@ -25,18 +25,18 @@ image_name="$repo_name:$tag_name"
 echo "https://x-access-token:${GITHUB_TOKEN}@github.com" > $PROJECT_ROOT/git-credentials.txt
 
 docker build \
-  --secret id=git-credentials,src=$PROJECT_ROOT/git-credentials.txt \
   -t "$image_name" \
   -f $CURRENT_DIR/Dockerfile \
   --build-arg RUST_VERSION=${RUST_VERSION} \
-  --build-arg PROTOC_VERSION=${PROTOC_VERSION} \
   $PROJECT_ROOT
 
 if [ -e $PROJECT_ROOT/git-credentials.txt ]; then
   rm -rf $PROJECT_ROOT/git-credentials.txt
 fi
 
-# login ECR
-# aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${REGISTRY_URL}
+docker run --rm -it -d --name tmp_noticube -e "SLACK_BOT_TOKEN=foo" -e "SLACK_CHANNEL_ID=bar" ${image_name}
 
-# docker push $image_name
+docker cp tmp_noticube:/usr/local/bin/noticube ${PROJECT_ROOT}/target/noticube
+docker rm -f tmp_noticube
+
+docker push $image_name
